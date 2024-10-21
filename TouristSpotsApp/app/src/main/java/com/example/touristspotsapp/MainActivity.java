bb2020afb5c60d569cb053e09f2a0f7851f80a8a
noone2097 <rafaelmiano1120@gmail.com>
2024-10-21 09:32:50 +0800

    added finish function

--- TouristSpotsApp/app/src/main/java/com/example/touristspotsapp/MainActivity.java
+++ TouristSpotsApp/app/src/main/java/com/example/touristspotsapp/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.touristspotsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,11 +21,32 @@ public class MainActivity extends AppCompatActivity {
         contactButton = findViewById(R.id.contactButton);
         logoutButton = findViewById(R.id.logoutButton);
 
-        spotsButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TouristSpotsListActivity.class)));
-        souvenirButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SouvenirListActivity.class)));
-        feedbackButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, FeedbackActivity.class)));
-        aboutButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
-        contactButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ContactUsActivity.class)));
+
+        spotsButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        souvenirButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        feedbackButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        aboutButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
+        contactButton.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
         logoutButton.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
--- TouristSpotsApp/app/src/main/java/com/example/touristspotsapp/TouristSpotsListActivity.java
+++ TouristSpotsApp/app/src/main/java/com/example/touristspotsapp/TouristSpotsListActivity.java
@@ -2,8 +2,6 @@ package com.example.touristspotsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
